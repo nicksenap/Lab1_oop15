c88180624fd94952ec3a0457dcf4cddb5a23da1a
nicksenap <nsong81@gmail.com>
2015-01-25 20:50:28 +0100

    Uppdate 2
    ”kommenterade”

--- src/currency2.java
+++ src/currency2.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 
 public class currency2 {
     public static void main (String args[]) throws IOException {
-        //This i
-        // s the
+        //This is the main method to the program.
         String val = null;
         String var = null;
         String mot = null;
@@ -17,25 +16,26 @@ public class currency2 {
         catch (ArrayIndexOutOfBoundsException exception){
             System.out.println("Usage: java Converter currency amount [currency]");
             System.exit(0);
-        }
+        }// If the "args" is shorter than 2, program will throw a exception and break.
 
-        if (args.length == 3){
+        if (args.length == 3){  //If user hasn't give the third course,
+                                // program will use "SEK" as default.
             mot = args[2];
         }else{
             mot = "SEK";
         }
-        Scanner sc=new Scanner(new File("valutor.txt"));
-        List<String>valuta= new ArrayList<String>();
+        Scanner sc=new Scanner(new File("valutor.txt"));// Use Scanner method to read the txt file.
+        List<String>valuta= new ArrayList<String>();  //Create 3 empty Array list.
         List<String>vardet= new ArrayList<String>();
         List<String>resten= new ArrayList<String>();
         while(sc.hasNext()){
             String rad=sc.nextLine();
-            Scanner sc2=new Scanner(rad);
+            Scanner sc2=new Scanner(rad);   //Read in the contents and add them to the Array lists.
             valuta.add(sc2.next());
             vardet.add(String.valueOf(sc2.next()));
             resten.add(sc2.nextLine());
         }
-        int index_val = valuta.indexOf(val);
+        int index_val = valuta.indexOf(val);  //get the index of the currency in user's input.
         int index_mot = valuta.indexOf(mot);
         if (index_mot == -1){
             System.out.println("'"+mot+"'"+"is not a recognized currency.");
@@ -44,10 +44,10 @@ public class currency2 {
         }else {
             Double res_sek = (Double.parseDouble(vardet.get(index_val)))*(Double.parseDouble(var));
             Double res = res_sek/(Double.parseDouble(vardet.get(index_mot)));
-            System.out.println(var+" "+val+" = "+String.format("%.2f",res)+" "+mot);}
+            System.out.println(var+" "+val+" = "+String.format("%.2f",res)+" "+mot);} //Do the output.
 
-    }
-}
+    } //End of main method.
+} // End of class
 
 
 
