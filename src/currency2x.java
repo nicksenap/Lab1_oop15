773c467dac9f10bc08b52385a52ce3b153dde08d
nicksenap <nsong81@gmail.com>
2015-01-26 10:40:15 +0100

    TEST3

--- src/currency2x.java
+++ src/currency2x.java
@@ -12,37 +12,39 @@ public class currency2x {
             System.exit(0);
         }
         //This is the main method to the program.
-        String val = args[0];
-        String var = args[1];
+        String val = args[0].toUpperCase();
+        String var = args[1].toUpperCase();
         String mot = null;
         // If the "args" is shorter than 2, program will throw a exception and break.
 
-        if (args.length == 3){  //If user hasn't give the third course,
-            // program will use "SEK" as default.
-            mot = args[2];
-        }else {
-            mot = "SEK";
-        }
+
         Scanner sc=new Scanner(new File("valutor.txt"));// Use Scanner method to read the txt file.
-        List<String>valuta= new ArrayList<String>();  //Create 3 empty Array list.
+        List<String>valuta= new ArrayList<String>();  //Create 3 empty Array list, for each post in the data file.
         List<String>vardet= new ArrayList<String>();
         List<String>resten= new ArrayList<String>();
         while(sc.hasNext()){
             String rad=sc.nextLine();
             Scanner sc2=new Scanner(rad);   //Read in the contents and add them to the Array lists.
-            valuta.add(sc2.next());
+            valuta.add(sc2.next().toUpperCase());
             vardet.add(String.valueOf(sc2.next()));
-            resten.add(sc2.nextLine());
+            resten.add(sc2.nextLine().substring(1));
+        }
+        if (args.length == 3){  //If user hasn't give the third course,
+            // program will use "SEK" as default.
+            mot = args[2];
+        }else {
+            mot = valuta.get(0);
         }
         int index_val = valuta.indexOf(val);  //get the index of the currency in user's input.
         int index_mot = valuta.indexOf(mot);
-        if (index_mot == -1){
+        if (index_mot == -1){   // Are the currencies valid?
             System.out.println("'"+mot+"'"+"is not a recognized currency.");
         }else if(index_val == -1){
             System.out.println("'"+val+"'"+" "+"is not a recognized currency.");
         }else {
             Double res_sek = (Double.parseDouble(vardet.get(index_val)))*(Double.parseDouble(var));
             Double res = res_sek/(Double.parseDouble(vardet.get(index_mot)));
+            System.out.println( "From " + resten.get(index_val) + " to "+" "+resten.get(index_mot)+".");
             System.out.println(var+" "+val+" = "+String.format("%.2f",res)+" "+mot);} //Do the output.
 
     } //End of main method.
