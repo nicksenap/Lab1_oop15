a8e9933116377461312ee9e9f7181a6d6233a42f
nicksenap <nsong81@gmail.com>
2015-01-26 11:58:19 +0100

    final

--- src/currency2x.java
+++ src/currency2x.java
@@ -4,6 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+class CurrencyConverter {
+    private ArrayList<String> names, descs;
+    private ArrayList<Double> vals;
+
+    // Creates a converter class based on an input file.
+    public CurrencyConverter( String file ) throws Exception {
+	names= new ArrayList<String>();  //Create 3 empty Array list.
+        descs = new ArrayList<String>();
+        vals = new ArrayList<Double>();
+
+	Scanner sc = new Scanner( new File( file ) );
+        while(sc.hasNext()){
+            String rad=sc.nextLine();
+            Scanner sc2=new Scanner(rad);   //Read in the contents and add them to the Array lists.
+            names.add(sc2.next().toUpperCase());
+	    String val = sc2.next();
+	    try {
+		vals.add( Double.parseDouble( val ) );
+	    }
+	    catch( Exception e ) {
+		throw new Exception();
+	    }
+			  
+           descs.add(sc2.nextLine());
+        }
+    }
+
+    // Converts one currency to another; returns a nice string explaining the result.
+    public String convert( Double amount, String from, String to ) {
+	from = from.toUpperCase();
+	to = to.toUpperCase();
+
+	Integer ind_from = names.indexOf( from ), ind_to = names.indexOf( to );
+
+	if( ind_from == -1 )
+	    return from + " is an invalid currency.";
+	if( ind_to == -1 )
+	    return to + " is an invalid currency.";
+
+	return "Converting from " + descs.get( ind_from ) + " to " + descs.get ( ind_to ) + ":\n" +
+	    String.format( "%.2f", amount ) + " " + from + " = " +
+	    String.format( "%.2f", amount * vals.get( ind_from ) / vals.get( ind_to ) );
+    }
+
+    // Converts one currency to the main currency; returns a nice string explaining the result.
+    public String convert( Double amount, String from ) {
+	return convert( amount, from, names.get( 0 ) );
+    }
+}
 
 public class currency2x {
     public static void main (String args[]) throws IOException {
@@ -11,41 +60,37 @@ public class currency2x {
             System.out.println("Usage: java Converter currency amount [currency]");
             System.exit(0);
         }
-        //This is the main method to the program.
-        String val = args[0].toUpperCase();
-        String var = args[1].toUpperCase();
-        String mot = null;
-        // If the "args" is shorter than 2, program will throw a exception and break.
 
+	// Create a converter:
+	CurrencyConverter conv = null;
 
-        Scanner sc=new Scanner(new File("valutor.txt"));// Use Scanner method to read the txt file.
-        List<String>valuta= new ArrayList<String>();  //Create 3 empty Array list, for each post in the data file.
-        List<String>vardet= new ArrayList<String>();
-        List<String>resten= new ArrayList<String>();
-        while(sc.hasNext()){
-            String rad=sc.nextLine();
-            Scanner sc2=new Scanner(rad);   //Read in the contents and add them to the Array lists.
-            valuta.add(sc2.next().toUpperCase());
-            vardet.add(String.valueOf(sc2.next()));
-            resten.add(sc2.nextLine().substring(1));
-        }
-        if (args.length == 3){  //If user hasn't give the third course,
-            // program will use "SEK" as default.
-            mot = args[2];
-        }else {
-            mot = valuta.get(0);
-        }
-        int index_val = valuta.indexOf(val);  //get the index of the currency in user's input.
-        int index_mot = valuta.indexOf(mot);
-        if (index_mot == -1){   // Are the currencies valid?
-            System.out.println("'"+mot+"'"+"is not a recognized currency.");
-        }else if(index_val == -1){
-            System.out.println("'"+val+"'"+" "+"is not a recognized currency.");
-        }else {
-            Double res_sek = (Double.parseDouble(vardet.get(index_val)))*(Double.parseDouble(var));
-            Double res = res_sek/(Double.parseDouble(vardet.get(index_mot)));
-            System.out.println( "From " + resten.get(index_val) + " to "+" "+resten.get(index_mot)+".");
-            System.out.println(var+" "+val+" = "+String.format("%.2f",res)+" "+mot);} //Do the output.
+	try {
+	    conv = new CurrencyConverter( "valutor.txt" );
+	}
+	catch( Exception e ) {
+	    System.out.println( "Error: corrupted data file." );
+	    System.exit( 0 );
+	    return;
+	}
+
+	Double amt;
+	try {
+	    amt = Double.parseDouble( args[1] );
+	}
+	catch( Exception e ) {
+	    System.out.println( "The amount must be a number." );
+	    System.exit( 0 );
+	    return;
+	}
+
+	String ret;
+
+	if( args.length == 3 ) 
+	    ret = conv.convert( amt, args[0], args[2] );
+	else
+	    ret = conv.convert( amt, args[0] );
+
+	System.out.println( ret );
 
     } //End of main method.
 } // End of class
